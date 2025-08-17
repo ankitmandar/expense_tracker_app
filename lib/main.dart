80802e85d68fc56b7906798f080c94faf3819359
Ankit Mandar <ankit.mandar.1@gmail.com>
2025-08-17 23:54:47 +0545

    Expense Tracker App with Provider

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:expense_tracker_app/screens/mainscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: Mainscreen(),
+    );
+  }
+}
